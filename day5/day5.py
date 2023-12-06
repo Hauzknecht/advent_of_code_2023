4d91ba711054277bd5c218e7e1965b86786653a7
Hauzknecht <milan.prochaz@gmail.com>
2023-12-06 07:59:58 +0100

    day5 part 1

---
+++ day5/day5.py
@@ -0,0 +1,24 @@
+file = open("input.txt", "r")
+segments = file.read().split("\n\n")
+seeds = [ int(x) for x in segments[0].split(":")[1].strip().split(" ")]
+mapping = []
+for segment in segments[1:]:
+    segment = segment.split("\n")
+    tmp = []
+    for line in segment[1:]:
+        tmp1 = tuple(int(x) for x in line.split(" "))
+        tmp.append(tmp1)
+    mapping.append(tmp)
+
+def src_dest(seed):
+    for _map in mapping:
+        for _range in _map:
+            if seed >= _range[1] and seed < _range[1]+_range[2]:
+                seed = _range[0] + seed - _range[1]
+                break
+    return seed
+
+destinations = []
+for seed in seeds:
+    destinations.append(src_dest(seed))
+print("Minimal destination is", min(destinations))
